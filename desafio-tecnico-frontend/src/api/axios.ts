f4990365ac4e23a41db3838c5bc458aa88cbde6b
Luciano da Rocha Schwalm <lucischwalm@MacBook-Air-de-Luciano-2.local>
2024-06-19 20:40:13 -0300

    renaming folders

--- desafio-tecnico-frontend/src/api/axios.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'http://localhost:3500/', // Altere para a URL do seu backend
-  timeout: 1000,
-  headers: { 'Content-Type': 'application/json' }
-});
-
-export default instance;
--- desafio-tecnico-frontend/src/dto/contentCreator.tsx
+++
@@ -1,9 +0,0 @@
-export interface ContentCreatorDto {
-    id: number,
-    nick: string;
-    email?: string;
-    followersYtb: number;
-    country: string;
-    lastVideoInAWeek: boolean;
-    isChecked?: boolean;
-  }
